aa5d41165c32e1116d0ad498a05451e84d298a2a
zydou <git@zydou.me>
2023-08-29 16:41:32 +0800

    feat: use tailored texlive docker image

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## This script will be executed after the devcontainer is created.
+## You can add additional packages, run other scripts, etc. here.
+
+## Install system packages
+# sudo apt-get update
+# sudo apt-get install -y system_package_name
+
+## Update texlive packages
+# sudo tlmgr update texlive_package_name
